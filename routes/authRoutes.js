0b3d253868077232acf59013adcdf250311f266a
sanjay <sanjay.india98886@gmail.com>
2026-07-15 08:25:50 -0700

    fix casing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware'); 
+const { protect } = require('../middleware/authmiddleware');
 const bcrypt = require('bcryptjs'); 
 const User = require('../models/User'); 
 
